956fc13a3f9b386b0e47911aa0c8d82fb1b7d784
Matt Farmer <mrfarmer777@gmail.com>
2018-08-03 06:14:47 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,6 +2,16 @@
 var state=["","","","","","","","",""]
 var squares=[];
 var turn=0;
+var winCombos=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
 
 $(function(){
   squares=$("td").toArray();

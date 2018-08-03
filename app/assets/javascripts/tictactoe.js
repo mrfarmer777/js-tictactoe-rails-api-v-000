bd97b904437abef3573cab07893ebc5ee2b13d91
Matt Farmer <mrfarmer777@gmail.com>
2018-08-03 04:56:15 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,13 @@
 // Code your JavaScript / jQuery solution here
+$(function(){
+  $('td').on('click',function(){
+    var move=player();
+    $(this).text(move);
+  })
+})
+
+
+
+function player(){
+  
+}
